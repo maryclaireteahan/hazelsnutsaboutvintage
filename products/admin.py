45315a03cda9e191c34411c7a01c02d9fda69e2b
maryclaireteahan <maryclaireteahan@gmail.com>
2023-12-31 17:47:24 +0000

    feat: add products, models, admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Product, Category, Brand
 # Register your models here.
+
+admin.site.register(Product)
+
+admin.site.register(Brand)
+
+admin.site.register(Category)
