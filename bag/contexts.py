7bfd8a84277041f38ec4b90cecdb87dfbf441907
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-01 19:50:28 +0000

    feat: add delivery threshold to base.html

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,14 +1,11 @@
-from django.conf import settings
 from decimal import Decimal
-
+from django.conf import settings
 
 def bag_contents(request):
-    """ Ensures that the bag contents are available when rendering every page """
-    
+
     bag_items = []
     total = 0
     product_count = 0
-    
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -16,7 +13,7 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-        
+    
     grand_total = delivery + total
     
     context = {
@@ -28,5 +25,5 @@ def bag_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-    
+
     return context
