12506fbd8cccf61561864ac74a399ad8814d8ce5
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-04 15:00:31 +0000

    feat: update webhook_handlers.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -11,6 +11,22 @@ class StripeWH_Handler:
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
+            content=f'Unhandled Webhook received: {event["type"]}',
+            status=200)
+    
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+    
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
     
