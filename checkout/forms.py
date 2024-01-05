c121ac1cf0f4829a228c8633298bc6cacff19687
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-05 20:41:10 +0000

    feat: update form.py in checkout app

--- checkout/forms.py
+++ checkout/forms.py
@@ -14,7 +14,6 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
