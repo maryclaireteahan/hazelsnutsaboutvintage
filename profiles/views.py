dfedf257cd54889b025957b7b184338b8d5c448e
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-05 22:49:28 +0000

    feat: add style for allauth templates to base.css

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,14 @@
-from multiprocessing import context
-from re import template
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 def profile(request):
     """ A view to return the profile page """
+    profile = get_object_or_404(UserProfile, user=request.user)
     
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
     
     return render(request, 'profiles/profile.html')
