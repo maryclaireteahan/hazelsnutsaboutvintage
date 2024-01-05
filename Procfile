6b1081d02248148d3e66617b89c5678855beb299
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-05 01:33:42 +0000

    feat: add procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mteahan-hazelsnutsaboutvintage.wsgi:application
--- hazelsnutsaboutvintage/settings.py
+++ hazelsnutsaboutvintage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from unittest.mock import DEFAULT
 from pathlib import Path
  
@@ -123,13 +124,17 @@ WSGI_APPLICATION = 'hazelsnutsaboutvintage.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = { 
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    } 
 
 
 # Password validation
