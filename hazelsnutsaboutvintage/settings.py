e7b130799195c80edff14a4eae6114e05f57f78b
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-05 18:34:39 +0000

    fix: remove unneeded import from settings.py

--- hazelsnutsaboutvintage/settings.py
+++ hazelsnutsaboutvintage/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 import dj_database_url
-from unittest.mock import DEFAULT
 from pathlib import Path
  
 if os.path.exists("env.py"):
