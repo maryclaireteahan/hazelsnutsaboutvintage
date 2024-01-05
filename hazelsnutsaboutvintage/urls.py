2720dc630d027913ecee06e2519c031560a567a5
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-05 21:41:08 +0000

    feat: add profile.html template to profiles app

--- hazelsnutsaboutvintage/urls.py
+++ hazelsnutsaboutvintage/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('event/', include('events.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
