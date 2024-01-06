74cc63789b2dc4fd019f6f1cc4981f81e1895c89
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-06 17:46:33 +0000

    feat: add login req for views.py in profiles & products

--- products/views.py
+++ products/views.py
@@ -1,32 +1,33 @@
-import re
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.db.models import Q
 from django.contrib import messages
-from .models import Product, Category, Brand
-from django.db.models.functions import Lower 
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Product, Category
 from .forms import ProductForm
+
+# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-    query = None 
+    query = None
     categories = None
-    brands = None
     sort = None
     direction = None
 
     if request.GET:
         if 'sort' in request.GET:
-            sortkey = request.GET['sort'] 
+            sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-                
-            if 'direction' in request.GET: # This is for the sort direction
+            if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
@@ -36,38 +37,33 @@ def all_products(request):
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-            
-        if 'brand' in request.GET:
-            brands = request.GET['brand'].split(',')
-            products = products.filter(brand__name__in=brands)
-            brands = Brand.objects.filter(name__in=brands)
-            
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(brand__name__icontains=query) | Q(category__name__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'{sort}_{direction}' # This is for the sort direction
-    
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
-        'search_term': query, # This is for the search bar to keep the search term in the search bar after the search is done
+        'search_term': query,
         'current_categories': categories,
-        'current_brands': brands,
-        'current_sorting': current_sorting, # This is for the sort direction
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
 
+
 def product_detail(request, product_id):
     """ A view to show individual product details """
-    
+
     product = get_object_or_404(Product, pk=product_id)
-    
+
     context = {
         'product': product,
     }
@@ -75,31 +71,42 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """ Add a product to the store """
-    if request.method=='POST':
-        form=ProductForm(request.POST, request.FILES)
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save()
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
-    else:    
-        form=ProductForm()
+    else:
+        form = ProductForm()
         
-    template='products/add_product.html'
-    context={
-        'form':form,
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
     }
-        
+
     return render(request, template, context)
 
+
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
-    if request.method=='POST':
-        form=ProductForm(request.POST, request.FILES, instance=product)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully updated product!')
@@ -109,17 +116,23 @@ def edit_product(request, product_id):
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
-        
+
     template = 'products/edit_product.html'
     context = {
         'form': form,
         'product': product,
     }
-    
+
     return render(request, template, context)
 
+
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
