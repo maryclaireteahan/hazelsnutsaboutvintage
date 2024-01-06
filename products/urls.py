20b9768dc0dc89f6fcde64a8f41f73a3cd36a351
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-06 16:07:21 +0000

    feat: add edit functionality to products in views.py & urls.py

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
-    # path('edit/', views.edit_product, name='edit_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     # path('delete/', views.delete_product, name='delete_product'),
     
 ]
