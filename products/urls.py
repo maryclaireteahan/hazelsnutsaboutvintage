80b860fc3bf5c32e46cf7fd9a8cd887308d9e01c
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-06 15:30:15 +0000

    feat: update urls.py in product app for CRUD

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>/', views.product_detail, name='product_detail')
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+    path('edit/', views.edit_product, name='edit_product'),
+    path('delete/', views.delete_product, name='delete_product'),
     
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from django.contrib import messages
 from .models import Product, Category, Brand
 from django.db.models.functions import Lower 
+from .forms import ProductForm
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -72,3 +73,14 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
+
+def add_product(request):
+    """ Add a product to the store """
+    
+    form=ProductForm()
+    template='products/add_product.html'
+    context={
+        'form':form,
+    }
+        
+    return render(request, template, context)
