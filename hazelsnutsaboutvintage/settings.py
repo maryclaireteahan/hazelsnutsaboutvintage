80fb9cfa10de3058f59ea85a8f8834acb8f18e03
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-06 20:06:47 +0000

    featL set debug to false in settings.py

--- hazelsnutsaboutvintage/settings.py
+++ hazelsnutsaboutvintage/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ	
+DEBUG = False	
 
 ALLOWED_HOSTS = ['mteahan-hazelsnutsaboutvintage-c846730bed11.herokuapp.com', 'localhost', '127.0.0.1']
 
