d4d1a1b219ae848118f85f0c44cda8440e2ab880
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-06 15:07:06 +0000

    feat: add forms.py to products app

--- hazelsnutsaboutvintage/settings.py
+++ hazelsnutsaboutvintage/settings.py
@@ -187,6 +187,7 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY','')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY','')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET','')
+DEFAULT_FROM_EMAIL = 'hazelsnutsaboutvintage@example.com'
 
 
 # Default primary key field type
---
+++ products/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Product, Category, Size, Brand
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        sizes = Size.objects.all()
+        brands = Brand.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        friendly_sizes = [(s.id, s.get_friendly_name()) for s in sizes]
+        friendly_brands = [(b.id, b.get_friendly_name()) for b in brands]
+        
+        self.fields['category'].choices = friendly_names
+        self.fields['size'].choices = friendly_sizes
+        self.fields['brand'].choices = friendly_brands
+        
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
