dc884622e0667db9ea07cf9d1a3929a5d350f3cf
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-06 20:04:51 +0000

    feat: change image in models.py product app

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Category(models.Model):
@@ -45,7 +46,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = CloudinaryField('image', null=True, blank=True)
     image_alt = models.CharField(max_length=254, default='image alt')
     size = models.ForeignKey('Size', null=True, blank=True, on_delete=models.SET_NULL)
     
