9fdcf19ee0c5e0564b008e8b8d314ff40a04d0bf
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-08 20:14:35 +0000

    feat: add barcode dependency

--- coupon/models.py
+++ coupon/models.py
@@ -1,3 +1,28 @@
+import uuid
+
+
 from django.db import models
 
 # Create your models here.
+class Coupon(models.Model):
+    """ A model for coupon """
+    code = models.CharField(max_length=20, unique=True)
+    barcode_image = models.ImageField(upload_to='barcode_images', blank=True, null=True)    
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    
+    def _generate_order_number(self):
+        """ Generate a random, unique order number using UUID """
+        return uuid.uuid4().hex.upper()
+    
+    class Meta:
+        verbose_name_plural = 'Coupon'
+    def __str__(self):
+        return self.code
+    
+    class Meta:
+        """
+        Order posts from newest to oldest
+        """
+        ordering = ['-created_on']
+        
