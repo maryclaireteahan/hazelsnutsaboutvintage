5924b54a736f0e56fe0ac3394d1cf00918eba67c
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-08 19:54:45 +0000

    feat: add coupon app

--- feedback/models.py
+++ feedback/models.py
@@ -5,11 +5,17 @@ class Feedback(models.Model):
     """ A model for feedback """
     subject = models.CharField(max_length=254, default='')
     message = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     
     class Meta:
         verbose_name_plural = 'Feedback'
     def __str__(self):
         return self.subject
     
+    class Meta:
+        """
+        Order posts from newest to oldest
+        """
+        ordering = ['-created_on']
+
     
--- feedback/views.py
+++ feedback/views.py
@@ -6,47 +6,36 @@ from .forms import FeedbackForm
 
 def feedback(request):
     """ A view to handle feedback submission """
-    
+    # Assuming you want to display the latest feedback
+    feedback = Feedback.objects.latest('created_at')  # Assuming 'created_at' is the timestamp field
+    context = {
+        'feedback': feedback,
+        'on_feedback_page': True,
+    }
+    return render(request, 'feedback/feedback.html', context)
+
+def all_feedback(request):
     if request.method == 'POST':
         feedback_form = FeedbackForm(request.POST)
         if feedback_form.is_valid():
             feedback_form.save()
             messages.success(request, 'Successfully added feedback!')
-            return redirect(reverse('home'))  # Redirect to the home page after adding feedback
         else:
             messages.error(request, 'Failed to add feedback. Please ensure the form is valid.')
+        return redirect(request.META.get('HTTP_REFERER'))
     else:
-        feedback_form = FeedbackForm()
-        
-    context = {
-        'feedback_form': feedback_form,
-    }
-    
-    return render(request, 'feedback/feedback.html', context)
-
-
-def all_feedback(request):
-    """ A view to show all feedback """
-
-    feedback = Feedback.objects.all()
-
-    context = {
-        'feedback': feedback,
-        'on_feedback_page': True,
-    }
-
-
-    return render(request, 'feedback/feedback.html', context)
-
+        feedback = Feedback.objects.all()  # Get all feedback objects
+        context = {
+            'feedback': feedback,
+            'on_feedback_page': True,
+        }
+        return render(request, 'feedback/feedback.html', context)
 
 def feedback_detail(request, feedback_id):
     """ A view to show individual feedback details """
-
     feedback = get_object_or_404(Feedback, pk=feedback_id)
-
     context = {
         'feedback': feedback,
         'on_feedback_page': True,
     }
-
     return render(request, 'feedback/feedback_detail.html', context)
