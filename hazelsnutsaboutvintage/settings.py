d6a7c3be7da4ba20d40d016525557ed5a79560b4
maryclaireteahan <maryclaireteahan@gmail.com>
2024-01-09 21:25:00 +0000

    feat: add red-button class to necessary templates

--- hazelsnutsaboutvintage/settings.py
+++ hazelsnutsaboutvintage/settings.py
@@ -205,7 +205,7 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER','')
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD','')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS','')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
